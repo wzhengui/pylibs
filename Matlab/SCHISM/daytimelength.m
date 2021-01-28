1b2866f6eb1a367d2e5b839a8f8386b834542dc0
Zhengui Wang <wzehngui@gmail.com>
2021-01-28 16:04:05 -0500

    delete and rename Matlab and Frotran scripts

--- Matlab/SCHISM/daytimelength.m
+++
@@ -1,8 +0,0 @@
-function f=daytimelength(Lat,Doy)
-%calculate daytime length
-%D=DaytimeLength(Lat,Doy)
-%Lat: latitude, Doy: (1-365), sunrise=12-D/2, sunset=12+D/2
-P=asin(0.39795*cos(0.2163108 + 2*atan(0.9671396*tan(0.00860*(Doy-186)))));
-T=(sin(0.8333*pi/180)+sin(Lat*pi/180)*sin(P))/cos(Lat*pi/180)./cos(P);
-f=24-(24/pi)*acos(T);
-end
--- Matlab/SCHISM/yeardays.m
+++
@@ -1,5 +0,0 @@
-function n=yeardays(Yr)
-%return number of days in a year;
-%n=yeardays(Year);
-n=datenum(Yr+1,1,1)-datenum(Yr,1,1);
-end
