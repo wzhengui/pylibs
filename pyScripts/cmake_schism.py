5f035a25086cb11807421e0624ba0ed29187fe01
Zhengui Wang <wzhengui@gmail.com>
2022-04-07 11:10:17 -0400

    1). minor edit on cmake_schism.py; 2). minor edit on James job command

--- pyScripts/cmake_schism.py
+++ pyScripts/cmake_schism.py
@@ -8,7 +8,8 @@ from pylib import *
 #Inputs: 
 #mods: OLDIO,PREC_EVAP,GOTM,HA,MARSH,SED2D,WWW,ICM,MICM,GEN,AGE,ECO,ICM,PH,COSINE,FIB,SED,FABM,ANALYSIS
 #-----------------------------------------------------------------------------------------------
-modules=['OLDIO',]
+modules=['OLDIO', 'ICM', 'PREC_EVAP'] 
+#modules=['OLDIO','COSINE']
 
 #directory of schism/fabm code
 schism='~/schism'; fabm='~/fabm'
@@ -18,7 +19,9 @@ schism='~/schism'; fabm='~/fabm'
 #-----------------------------------------------------------------------------------------------
 #write SCHSI.local.build
 schism=schism.replace('~',os.path.expanduser("~")); fabm=fabm.replace('~',os.path.expanduser("~")); 
-fname='{}/cmake/SCHISM.local.build'.format(schism)
+fname='{}/cmake/SCHISM.local.build'.format(schism) 
+
+fid=open(fname,'r'); lines=fid.readlines(); fid.close() #save original file
 rewrite(fname,replace=['ON','OFF'],include=['USE_','PREC_EVAP','OLDIO'])
 rewrite(fname,replace=['OFF','ON'],include=[i if i in ['OLDIO','PREC_EVAP'] else 'USE_{}'.format(i) for i in modules])
 rewrite(fname,replace=[],include=['FABM_BASE'])
@@ -28,11 +31,18 @@ rewrite(fname,append=['set( FABM_BASE {} CACHE STRING "Path to FABM base")\n'.fo
 host=os.getenv('HOST').split('.')[0]
 if host=='hurricane': host='whirlwind'
 
-#compile 
-if not fexist('{}/build'.format(schism)): os.mkdir('{}/build'.format(schism))
-os.system('cd {}/build; rm -rf *; cmake -C ../cmake/SCHISM.local.build -C ../cmake/SCHISM.local.{} ../src; make -j8 pschism'.format(schism,host))
+try: 
+   #compile 
+   if not fexist('{}/build'.format(schism)): os.mkdir('{}/build'.format(schism))
+   os.system('cd {}/build; rm -rf *; cmake -C ../cmake/SCHISM.local.build -C ../cmake/SCHISM.local.{} ../src; make -j8 pschism'.format(schism,host))
+   
+   #put tag number
+   sname=os.listdir('{}/build/bin'.format(schism))[0]
+   irev=command_outputs('cd {}; git log'.format(schism)).stdout.split('\n')[0].split()[1][:8]
+   os.system('cp {}/build/bin/{} ./{}.{}'.format(schism,sname,sname,irev)); 
 
-#put tag number
-sname=os.listdir('{}/build/bin'.format(schism))[0]
-irev=command_outputs('cd {}; git log'.format(schism)).stdout.split('\n')[0].split()[1][:8]
-os.system('cp {}/build/bin/{} ./{}.{}'.format(schism,sname,sname,irev)); 
+   #write original file
+   fid=open(fname,'w+'); fid.writelines(lines); fid.close() 
+except: 
+   #write original file
+   fid=open(fname,'w+'); fid.writelines(lines); fid.close()
