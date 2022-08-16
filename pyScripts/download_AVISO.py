435bafbb7df66dbdeb7fee6ae0f0e6b69b8ec5c7
kyungmin0616 <kmp06169@gmail.com>
2022-08-16 15:19:02 -0400

    AVISO

--- pyScripts/download_AVISO.py
+++ pyScripts/download_AVISO.py
@@ -1,13 +1,14 @@
+#!/usr/bin/env python3
 from ftplib import FTP
 from pylib import *
 
-
-StartT=datenum('2016-9-1'); EndT=datenum('2016-11-4') # time period to download
+################################################
+StartT=datenum('2016-9-1'); EndT=datenum('2016-9-4') # time period to download
 host='ftp-access.aviso.altimetry.fr' # domain name 
 user='kmpark19900616@gmail.com'; passwd = 'Y0PLwL' # user id and password
 wdir='/duacs-experimental/dt-phy-grids/multiscale_interpolation_alti_drifters/version_01_00'# target directory to download
-
 #ftp.dir() #list contents in current dir
+################################################
 
 # connect to host and go to target directory
 ftp = FTP(host)
@@ -22,6 +23,7 @@ sind=argsort(mti); mti=mti[sind]; fnames=fnames[sind]
 
 # dowload data
 for fname in fnames:
+    print('Downloading {}'.format(fname))
     with open(fname, "wb") as file:
-        ftp.retrbinary(f"RETR {filename}", file.write)
+        ftp.retrbinary(f"RETR {fname}", file.write)
 print('--------------Done-------------')
