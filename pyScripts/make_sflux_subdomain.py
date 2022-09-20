ace4ec37d17a0bac6182ae469c00a71af31aff99
Zhengui Wang <wzhengui@gmail.com>
2022-09-20 10:53:59 -0400

    update make_sflux_subdomain.py

--- pyScripts/make_sflux_subdomain.py
+++ pyScripts/make_sflux_subdomain.py
@@ -18,21 +18,19 @@ itag=1   #itag=[1 or 2],for sflux_air_itag.0691.nc
 #---------------------------------------------------------------------------
 if not os.path.exists(tdir): os.mkdir(tdir)
 fid=open('{}/sflux_inputs.txt'.format(tdir),'w+'); fid.write('&sflux_inputs\n \n/'); fid.close()
-mtime=arange(StartT,EndT+1); svars=['air','prc','rad']
+mtime=arange(StartT,EndT+1); svars=['air','prc','rad']; ix1s,ix2s,iy1s,iy2s=[],[],[],[]
 for irec,ti in enumerate(mtime):
-    #get date 
-    year=num2date(ti).year
-    month=num2date(ti).month
-    day=num2date(ti).day
+    pti=num2date(ti); year,month,day=pti.year,pti.month,pti.day
 
     #link each file
     for m,svar in enumerate(svars):
+        #NARR database
         fname='{}/{}_{:02}/narr_{}.{}_{:02d}_{:02d}.nc'.format(sdir,year,month,svar,year,month,day)
         bname='{}/sflux_{}_{}.{:04d}.nc'.format(tdir,svar,itag,irec)
-        print('reading: {}'.format(fname))
+        if day==1 and m==0: print('reading: {}'.format(fname))
 
         #compute indices of subdomaine
-        if irec==0 and m==0:  
+        if irec==0:
            C=ReadNC(fname,1);lon=array(C.variables['lon'][:]); lat=array(C.variables['lat'][:]); C.close(); 
            ny,nx=lon.shape; sind=near_pts(c_[mean(xm),mean(ym)],c_[lon.ravel(),lat.ravel()]); 
            iy,ix=[i[0] for i in unravel_index(sind,[ny,nx])]; ix1,ix2,iy1,iy2=ix,ix+1,iy,iy+1
@@ -47,46 +45,36 @@ for irec,ti in enumerate(mtime):
                if (ix1i==ix1)*(ix2i==ix2)*(iy1i==iy1)*(iy2i==iy2): break
 
            #shrink the box
-           for m in arange(4):
+           for n in arange(4):
                while True:
-                   if m==0: ix1=ix1+1
-                   if m==1: ix2=ix2-1
-                   if m==2: iy1=iy1+1
-                   if m==3: iy2=iy2-1
+                   if n==0: ix1=ix1+1
+                   if n==1: ix2=ix2-1
+                   if n==2: iy1=iy1+1
+                   if n==3: iy2=iy2-1
                    px=r_[lon[iy1:iy2,ix1],lon[iy2-1,(ix1+1):(ix2-1)],lon[iy1:iy2,ix2-1][::-1],lon[iy1,(ix1+1):(ix2-1)][::-1]]
                    py=r_[lat[iy1:iy2,ix1],lat[iy2-1,(ix1+1):(ix2-1)],lat[iy1:iy2,ix2-1][::-1],lat[iy1,(ix1+1):(ix2-1)][::-1]]
                    x1,x2=xm; y1,y2=ym; pts=c_[array([x1,x2,x2,x1]),array([y1,y1,y2,y2])]
                    if inside_polygon(pts,px,py).sum()!=4: 
-                      if m==0: ix1=ix1-1
-                      if m==1: ix2=ix2+1
-                      if m==2: iy1=iy1-1
-                      if m==3: iy2=iy2+1
+                      if n==0: ix1=ix1-1
+                      if n==1: ix2=ix2+1
+                      if n==2: iy1=iy1-1
+                      if n==3: iy2=iy2+1
                       break
-           #plot
-           #x1,x2=xm; y1,y2=ym; xi=array([x1,x2,x2,x1,x1]); yi=array([y1,y1,y2,y2,y1])
-           #lxi=lon[iy1:iy2,ix1:ix2].ravel()
-           #lyi=lat[iy1:iy2,ix1:ix2].ravel()
-           #plot(lon.ravel(),lat.ravel(),'k.',ms=1)
-           #plot(lxi,lyi,'g.',xi,yi,'r-'); show(block=False); sys.exit()
 
-        #read sflux 
-        C=ReadNC(fname)
-     
-        #change dimension
+           #save indices
+           ix1s.append(ix1); ix2s.append(ix2); iy1s.append(iy1); iy2s.append(iy2)
+
+        #read sflux, and change dimension
+        C=ReadNC(fname);  ix1,ix2,iy1,iy2=ix1s[m],ix2s[m],iy1s[m],iy2s[m]
         for n,dimname in enumerate(C.dimname):
             if dimname=='nx_grid': C.dims[n]=ix2-ix1
             if dimname=='ny_grid': C.dims[n]=iy2-iy1
 
         #change variables values
         for mvar in C.vars:
-            if mvar in ['time']: 
-               continue 
-            elif mvar in ['lon','lat']: 
+            if mvar in ['time']: continue
+            if mvar in ['lon','lat']:
                exec('C.{}.val=C.{}.val[iy1:iy2,ix1:ix2]'.format(mvar,mvar))
             else:
                exec('C.{}.val=C.{}.val[:,iy1:iy2,ix1:ix2]'.format(mvar,mvar))
-
-        #write sflux 
-        WriteNC(bname,C)
-
-
+        WriteNC(bname,C) #write sflux
