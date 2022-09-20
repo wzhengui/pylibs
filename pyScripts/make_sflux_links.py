0419def853af99deb1f2cc68da95bd43a33f67a3
Zhengui Wang <wzhengui@gmail.com>
2022-09-20 12:24:06 -0400

    use relative path in narr sflux

--- pyScripts/make_sflux_links.py
+++ pyScripts/make_sflux_links.py
@@ -22,7 +22,7 @@ for irec,ti in enumerate(mtime):
     year=num2date(ti).year; month=num2date(ti).month; day=num2date(ti).day
     for m,svar in enumerate(svars):
         fname='{}/{}_{:02}/narr_{}.{}_{:02d}_{:02d}.nc'.format(sdir,year,month,svar,year,month,day)
-        os.symlink(fname,'sflux_{}_{}.{:04d}.nc'.format(svar,itag,irec+1))
+        os.symlink(os.path.relpath(fname),'sflux_{}_{}.{:04d}.nc'.format(svar,itag,irec+1))
         if m==1 and day==1: print('    sflux: {:04d}-{:02d}-{:02d}'.format(year,month,day))
 #write sflux_inputs.txt
 fid=open('{}/sflux_inputs.txt'.format(tdir),'w+'); fid.write('&sflux_inputs\n \n/'); fid.close()
