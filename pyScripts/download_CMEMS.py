59644fb8ca472816fa43a4de5ed99da95da50e44
Kyungmin Park <kmp06169@gmail.com>
2023-04-27 16:20:45 -0700

    revised error

--- pyScripts/download_CMEMS.py
+++ pyScripts/download_CMEMS.py
@@ -7,10 +7,11 @@ from pylib import *
 # reanlaysis: https://catalogue.marine.copernicus.eu/documents/PUM/CMEMS-GLO-PUM-001-030.pdf
 
 ###############################################
-StartT,EndT=datenum(2022,1,1,fmt=1),datenum(2022,12,30,fmt=1) # time 
-tres='monthly' # hourly, daily, monthly. 
-xl=[-100,20] # longitude
-yl=[-25,65] #latitude
+StartT,EndT=datenum(2018,12,31,fmt=1),datenum(2020,1,1,fmt=1) # time 
+tres='daily' # hourly, daily, monthly. 
+xl=[-163,-146] # longitude
+yl=[16,26] #latitude
+
 dl=[0, 8727.917] #depth
 data='rana' # analysis:'GLOBAL_ANALYSISFORECAST_PHY_001_024-TDS'; reanlysis: GLOBAL_MULTIYEAR_PHY_001_030-TDS
 myid='kpark' #user id
@@ -54,14 +55,14 @@ if data=='ana':
            os.system(furl)
 
 elif data=='rana':
-   serv='GLOBAL_MULTIYEAR_PHY_001_030-TDS'
-   print('Downloading reanalysis data from CMEMS')
+       serv='GLOBAL_MULTIYEAR_PHY_001_030-TDS'
+       print('Downloading reanalysis data from CMEMS')
        # Temporal resolution definition
        if tres=='daily':
           dt=datetime.timedelta(hours=24); pid='P1D-m'
        elif tres=='monthly':
           dt=datetime.timedelta(days=31); pid='P1M-m'
-       else: print('Unrecognized temporal resolution --{}--.'.format(tres)); break
+       else: print('Unrecognized temporal resolution --{}--.'.format(tres)); 
        tlist=drange(StartT,EndT+dt,dt)
        dt=tlist[1]-tlist[0]
        # Download data
@@ -71,9 +72,9 @@ elif data=='rana':
            elif tres=='monthly':
               st=num2date(ti).strftime('%Y_%m_00_00')
            if os.path.isfile('cmems_{}.nc'.format(st)): print('Data on {} exsit'.format(st)); continue
-           furl='motuclient --motu https://nrt.cmems-du.eu/motu-web/Motu --service-id {} --product-id cmems_mod_glo_phy_my_0.083_{} --longitude-min {} --longitude-max {} --latitude-min {} --latitude-max {} --date-min "{}" --date-max "{}" --depth-min {} --depth-max {} --variable so --variable thetao --variable uo --variable vo --varibale zos --out-dir {} --out-name cmems_{}.nc --user {} --pwd {}'.format(serv,prod,pid,xl[0],xl[1],yl[0],yl[1],num2date(ti).strftime('%Y-%m-%d %H:%M:%S'),num2date(ti+dt*0.9).strftime('%Y-%m-%d %H:%M:%S'),dl[0],dl[1],sdir,st,myid,pswd)
-           #print(furl)
-          os.system(furl)
+           furl='motuclient --motu https://my.cmems-du.eu/motu-web/Motu --service-id {} --product-id cmems_mod_glo_phy_my_0.083_{} --longitude-min {} --longitude-max {} --latitude-min {} --latitude-max {} --date-min "{}" --date-max "{}" --depth-min {} --depth-max {} --variable so --variable thetao --variable uo --variable vo --variable zos --out-dir {} --out-name cmems_{}.nc --user {} --pwd {}'.format(serv,pid,xl[0],xl[1],yl[0],yl[1],num2date(ti).strftime('%Y-%m-%d %H:%M:%S'),num2date(ti+dt*0.9).strftime('%Y-%m-%d %H:%M:%S'),dl[0],dl[1],sdir,st,myid,pswd)
+           print(furl)
+           os.system(furl)
 
 else: print('Wrong data type. Choose ana or rana in data')
 
