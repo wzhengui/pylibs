d444ba673624b7c4cee702f5379b72fc584274ad
Zhengui Wang <wzhengui@gmail.com>
2023-08-08 10:45:53 -0400

    change pylibs name to pylibs_ocean

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
     setup(
-        name='pylibs',
+        name='pylibs_ocean',
         packages=[
           'pyUtility',
     	    'pyScripts',
