fd5bc45a02d975e61d010ad7694ca7cf069eded7
Zhengui Wang <wzhengui@gmail.com>
2023-08-08 11:31:37 -0400

    update version number

--- setup.py
+++ setup.py
@@ -10,12 +10,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
     	    'pyScripts',
         ],
         py_modules=['pylib'],
-        version='0.1.12',  # Ideally should be same as your GitHub release tag varsion
+        version='0.1.13',  # Ideally should be same as your GitHub release tag varsion
         package_data={'pyScripts': ['prj.npz','sflux_template.npz','Harmonic_Analysis']},
         description='python tools for ocean reserach',
         long_description='python libraries and utilities for data processing including the pre/post-processing about SCHISM models',
         author='Zhengui Wang',
         author_email='wzhengui@gmail.com',
+        url='https://github.com/wzhengui/pylibs',
         classifiers=[],
         install_requires=[
     	    'setuptools',
