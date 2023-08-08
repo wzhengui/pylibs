ea37e26df52fc8f872e32ad37701e3f7bced27e7
Zhengui Wang <wzhengui@gmail.com>
2023-08-08 10:42:02 -0400

    minor edit on setup.py

--- setup.py
+++ setup.py
@@ -12,10 +12,10 @@ with open("README.md", "r", encoding="utf-8") as fh:
         py_modules=['pylib'],
         version='0.1.12',  # Ideally should be same as your GitHub release tag varsion
         package_data={'pyScripts': ['prj.npz','sflux_template.npz','Harmonic_Analysis']},
-        description='python libraries and utilities for pre/post-processing SCHISM models',
-        long_description='python libraries and utilities for pre/post-processing SCHISM models',
+        description='python tools for ocean reserach',
+        long_description='python libraries and utilities for data processing including the pre/post-processing about SCHISM models',
         author='Zhengui Wang',
-        author_email='wangzg@vims.edu',
+        author_email='wzhengui@gmail.com',
         classifiers=[],
         install_requires=[
     	    'setuptools',
