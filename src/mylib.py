38e9f3b305bcb373207b2692f48add73d7f0eb7c
Zhengui Wang <wzhengui@gmail.com>
2025-06-12 14:57:17 -0400

    minor edit hpc command

--- src/mylib.py
+++ src/mylib.py
@@ -538,6 +538,7 @@ def get_qnode(qnode=None):
     if host in ['kuro.sciclone.wm.edu']: qnode='kuro'
     if host in ['gulf.sciclone.wm.edu']: qnode='gulf'
     if re.match('bo\w+.sciclone.wm.edu',host)!=None: qnode='bora'
+    if host.startswith('bo') and host.endswith('sciclone.wm.edu'): qnode='bora'
     if host in ['chesapeake.sciclone.wm.edu']: qnode='potomac'
     return qnode
 
