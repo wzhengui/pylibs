74a8a3d229f9fea9a9b1714e5de8e52debfadb74
Zhengui Wang <wzhengui@gmail.com>
2026-05-22 13:15:17 -0400

    update anim_tsunami.py

--- scripts/anim_tsunami.py
+++ scripts/anim_tsunami.py
@@ -10,29 +10,32 @@ mpl.use('agg'); close("all")
 #Inputs
 #--------------------------------------------------
 sdir='/sciclone/home/yinglong/DISKS/schism10/tsunami/Project_2024_NorthCoast_PTHA/RUN02c'  #run dir
-xm=[-124.538,-123.283]; ym=[45.416,46.726]  #subset region
-zlim=[-3,30]                                #elevation limit
-stacks=[1,24]                               #output stacks to be plotted
-nspool=10                                   #sub-sampling frequency within each stack (1 means all)
+xm=[-124.538,-123.75]; ym=[45.75,46.3]  #subset region
+zlim=[-3,15]                                #elevation limit
+stacks=[1,3]                                #output stacks to be plotted
+nspool=1                                    #sub-sampling frequency within each stack (1 means all)
 isobath=[0,100]                             #isobath
 bpfile='station.bp'                         #station for plotitng time series; commment out if not need
-delay=200                                   #animation step (milli-seconds)
+delay=500                                   #animation step (milli-seconds)
 cmap='coolwarm'                             #colormap
 fname='animation_tsunami'                   #animation file name
 nproc=32                                    #number of process
+hgrid=sdir+'/hgrid.gr3'                     #hgrid
+#time_cutoff=2500                            #cutoff time
 
 #--------------------------------------------------
 #read grid and output information
 #--------------------------------------------------
 istacks0=arange(stacks[0],stacks[1]+1); nstack=len(istacks0)
-p=read(sdir+'/param.nml',3); dt=int(p.dt); nrec0=int(p.ihfskip/p.nspool); nrec=int(nrec0/nspool)
+p=read(sdir+'/param.nml',3); dt=int(p.dt*p.nspool); nrec0=int(p.ihfskip/p.nspool); nrec=int(nrec0/nspool)
 istacks=tile(istacks0,[nrec,1]).T.ravel(); irecs=tile(arange(nrec)*nspool,[nstack,1]).ravel()
-its=arange(nstack*nrec); nt=len(its); mt=arange(nt)*dt*nspool
-#print('reading hgrid'); gd=read(sdir+'/hgrid.gr3'); print('done: reading hgrid')
-gd=read('hgrid.npz')
+its=arange(nstack*nrec); nt=len(its); mt=arange(nt)*dt*nspool+dt
+#fpt=mt<=time_cutoff; its=its[fpt]; mt=mt[fpt]; nt=len(its) #cutoff time
+print('reading hgrid'); gd=read(hgrid); print('done: reading hgrid')
+#gd=read('hgrid.npz')
 cxy=gd.compute_contour(isobath) #compute isobath
+mdata=get_basemap([10,10],xm,ym,dpi=1000)
 
-#if not fexist('hgrid.npz'): read(sdir+'/hgrid.gr3').save('hgrid.npz')
 #--------------------------------------------------
 #fread: extract time series @station; fplot: plot
 #--------------------------------------------------
@@ -47,17 +50,19 @@ def fplot(it):
 
     hf=figure(figsize=[10 if npt==0 else 20,10])
     if npt==0:
-       hp=gd.plot(1,elev,clim=zlim,cb_aspect=60,cb_pad=0.01,levels=100, cmap=cmap,bnd=1)
-       for xy in cxy: plot(*xy.T,'g',lw=0.5,alpha=0.65)  #isobath
+       hp=gd.plot(1,elev,clim=zlim,cb_aspect=60,cb_pad=0.01,levels=100, cmap=cmap,bnd=0)
+       for xy in cxy: plot(*xy.T,'k',lw=0.5,alpha=0.65)  #isobath
+       imshow(mdata,extent=[*xm,*ym],origin='lower',aspect='auto');
        setp(gca(),xlim=xm,ylim=ym)
        title('Elevation (m): {:d} s'.format(mt[it]),fontsize=14,fontweight='bold')
     else:
        #map
        subplot(1,2,1)
-       hp=gd.plot(1,elev,clim=zlim,cb_aspect=60,cb_pad=0.01,levels=100, cmap=cmap,bnd=1)
-       plot(*bp.xy.T,'g^',ms=12) #add bp pts
-       for xy in cxy: plot(*xy.T,'g',lw=0.5,alpha=0.65)  #isobath
-       for x,y,v in zip(bp.x,bp.y,bp.station): text(x,y,v,fontsize=12,color='g')
+       hp=gd.plot(1,elev,clim=zlim,cb_aspect=60,cb_pad=0.01,levels=100, cmap=cmap,bnd=0)
+       plot(*bp.xy.T,'ko',ms=10) #add bp pts
+       for xy in cxy: plot(*xy.T,'k',lw=0.5,alpha=0.65)  #isobath
+       for x,y,v in zip(bp.x,bp.y,bp.station): text(x-0.035*diff(xm),y,v,fontsize=12,color='w')
+       imshow(mdata,extent=[*xm,*ym],origin='lower',aspect='auto');
        setp(gca(),xlim=xm,ylim=ym)
        title('Elevation (m): {:d} s'.format(mt[it]),fontsize=14,fontweight='bold')
 
@@ -68,13 +73,16 @@ def fplot(it):
            plot(mt,0*mt,'k:',lw=0.1);plot(mt,my,'k-'); plot(mt[it],my[it],'ro',ms=10); plot([mt[it],mt[it]],ymt,'r')
 
            #note
-           gca().xaxis.grid('on'); xshift=-0.06 if (mt[-1]-mt[it]<0.2*dx) else 0.02
-           text(mt[it]+xshift*dx,mean(ymt),'{:d} s'.format(mt[it]),color='r',fontsize=12)
+           xshift=-0.08 if (mt[-1]-mt[it]<0.2*dx) else 0.02; yts=arange(int(ymt[0]),int(ymt[1])+2,2); 
+           yls=yts.astype('U') if len(yts)<10 else [str(k) if i%2==0 else '' for i,k in enumerate(yts)]
+           text(mt[it]+xshift*dx,mean(ymt),'{:0.1f}m\n{:d}s'.format(my[it],mt[it]),color='r',fontsize=12)
            if m==0: setp(gca(),xlim=xmt,ylim=ymt); xts=xticks()[0]; xls=[str(i) for i in xts]
-           setp(gca(),xticks=xts,xticklabels=xls if m==(npt-1) else [],xlim=xmt,ylim=ymt)
+           setp(gca(),xticks=xts,xticklabels=xls if m==(npt-1) else [],xlim=xmt,yticks=yts,yticklabels=yls,ylim=ymt)
            rtext(0.5,0.9,bp.station[m],fontsize=14,fontweight='bold',color='r')
+           gca().xaxis.grid('on'); gca().yaxis.grid('on')
     
     gcf().tight_layout()
+    #show(block=False); sys.exit()
     savefig('A_{:04}'.format(it),dpi=150); close()
 
 #--------------------------------------------------
